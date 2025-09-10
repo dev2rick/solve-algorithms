f3e1ae5d098bad7083167c03ebcc0f7fb795c844
rick <dev@2rick.com>
2025-09-11 08:39:43 +0900

    2884-알람 시계

---
+++ baekjoon/단계별/2.조건문/2884-알람 시계.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int h, m;
+    cin >> h >> m;
+    
+    if(m - 45 >= 0) {
+        cout << h << " " << m-45 << endl;
+    } else {
+        cout << (h+23) % 24 << " " << m+15 << endl;
+    }
+    return 0;
+}
