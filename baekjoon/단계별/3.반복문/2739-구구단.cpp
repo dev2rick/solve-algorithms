f302673bf98c652afe2842932e25f2742f5a6cc5
rick <dev@2rick.com>
2025-09-11 11:53:42 +0900

    2739-구구단

---
+++ baekjoon/단계별/3.반복문/2739-구구단.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int k;
+    cin >> k;
+    
+    for(int i=1; i<10; i++) {
+        cout << k << " * " << i << " = " << k*i << endl;
+    }
+    return 0;
+}
