62e55c7622ac64dd7387d56f5d567e4101a89248
rick <dev@2rick.com>
2025-09-12 10:09:44 +0900

    10950-A+B-3

---
+++ baekjoon/단계별/3.반복문/10950-A+B-3.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int k, a, b;
+    
+    cin >> k;
+    
+    for(int i=0; i<k; i++) {
+        cin >> a >> b;
+        cout << a+b << endl;
+    }
+    return 0;
+}
