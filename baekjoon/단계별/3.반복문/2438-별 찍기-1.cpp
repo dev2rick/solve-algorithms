48efe1934d82927b961e9860d4a83a752a734a6e
rick <dev@2rick.com>
2025-09-12 10:49:36 +0900

    2438-별 찍기-1

---
+++ baekjoon/단계별/3.반복문/2438-별 찍기-1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int k;
+    cin >> k;
+    
+    for(int i=1; i<=k; i++) {
+        for(int j=0; j<i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+        
+    return 0;
+}
