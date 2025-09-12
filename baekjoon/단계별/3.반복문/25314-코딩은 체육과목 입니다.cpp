bdbce039c4d6a8d5b588c38fb0c7d4b86a7990cc
rick <dev@2rick.com>
2025-09-12 10:34:11 +0900

    25314-코딩은 체육과목 입니다

---
+++ baekjoon/단계별/3.반복문/25314-코딩은 체육과목 입니다.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int k;
+    cin >> k;
+    
+    for(int i=0; i<k/4; i++) {
+        cout << "long ";
+    }
+    cout << "int\n";
+    return 0;
+}
