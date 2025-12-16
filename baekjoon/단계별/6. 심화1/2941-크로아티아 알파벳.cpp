3d931b7054a417dc2b3cd6a546e284663b0dad54
rick <dev@2rick.com>
2025-12-16 18:13:58 +0900

    2941-크로아티아 알파벳

--- baekjoon/단계별/6. 심화1/2941-크로아티아 알파벳.cpp
+++ baekjoon/단계별/6. 심화1/2941-크로아티아 알파벳.cpp
@@ -1,8 +1,24 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-
 int main() {
-    
+    string v[8] = {"c=","c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+    string s;
+    cin >> s;
+
+    int index;
+
+    for (int i = 0; i < 8; i++) {
+
+        while (1) {
+            index = s.find(v[i]);
+            if (index == string::npos)
+                break;
+            s.replace(index, v[i].length(), "a");
+        }
+    }
+
+    cout << s.size();
     return 0;
 }
